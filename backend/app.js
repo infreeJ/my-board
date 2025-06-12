b660360c725416a75682fe7504818440154813ce
infreeJ <kasious84@gmail.com>
2025-06-12 19:14:18 +0900

    SELECT문 완성

--- backend/app.js
+++ backend/app.js
@@ -17,13 +17,68 @@ const dbConfig = {
   connectString: 'localhost:1521/XE'
 };
 
+app.get('/users', async (req, res) => {
+  let connection;
+
+  try {
+    connection = await oracledb.getConnection(dbConfig);
+    const result = await connection.execute('SELECT * FROM USERS');
+    res.json(result.rows);
+    
+
+  } catch (err) {
+    console.error("DB 연결 또는 쿼리 에러:", err);
+    res.status(500).json({ error: 'DB 오류', message: err.message });
+  } finally {
+    if (connection) await connection.close();
+  }
+});
+
 app.get('/posts', async (req, res) => {
   let connection;
 
   try {
     connection = await oracledb.getConnection(dbConfig);
-    const result = await connection.execute('SELECT * FROM POSTS');
+    const result = await connection.execute(
+      `SELECT p.id, u.name, p.title, p.likes, p.views, p.created_at
+      FROM users u JOIN posts p ON u.id = p.user_id
+      ORDER BY p.id ASC`)
     res.json(result.rows);
+    
+    const postId = result.rows[0][0]
+    const userName = result.rows[0][1]
+    const postTitle = result.rows[0][2]
+    const postLikes = result.rows[0][3]
+    const postViews = result.rows[0][4]
+    const postCreated_at = result.rows[0][5]
+
+    console.log(postId);
+    console.log(userName);
+    console.log(postTitle);
+    console.log(postLikes);
+    console.log(postViews);
+    console.log(postCreated_at);
+    
+
+    // console.log("쿼리 결과:", result.rows[0][1]);
+    
+  } catch (err) {
+    console.error("DB 연결 또는 쿼리 에러:", err);
+    res.status(500).json({ error: 'DB 오류', message: err.message });
+  } finally {
+    if (connection) await connection.close();
+  }
+});
+
+app.get('/comments', async (req, res) => {
+  let connection;
+
+  try {
+    connection = await oracledb.getConnection(dbConfig);
+    const result = await connection.execute('SELECT * FROM COMMENTS');
+    res.json(result.rows);
+  
+    
   } catch (err) {
     console.error("DB 연결 또는 쿼리 에러:", err);
     res.status(500).json({ error: 'DB 오류', message: err.message });
