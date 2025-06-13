dd7000342228fe2306a0a1eae6901e21e4313faf
infreeJ <kasious84@gmail.com>
2025-06-13 13:04:32 +0900

    페이지 당 10개 글 출력하는 기능 구현 완료

--- backend/app.js
+++ backend/app.js
@@ -17,25 +17,10 @@ const dbConfig = {
   connectString: 'localhost:1521/XE'
 };
 
-app.get('/users', async (req, res) => {
-  let connection;
 
-  try {
-    connection = await oracledb.getConnection(dbConfig);
-    const result = await connection.execute('SELECT * FROM USERS');
-    res.json(result.rows);
-    
 
-  } catch (err) {
-    console.error("DB 연결 또는 쿼리 에러:", err);
-    res.status(500).json({ error: 'DB 오류', message: err.message });
-  } finally {
-    if (connection) await connection.close();
-  }
-});
-
-app.get('/posts', async (req, res) => {
-  let connection;
+app.get('/posts/:page', async (req, res) => {
+    let connection;
 
   try {
     connection = await oracledb.getConnection(dbConfig);
@@ -43,50 +28,23 @@ app.get('/posts', async (req, res) => {
       `SELECT p.id, u.name, p.title, p.likes, p.views, p.created_at
       FROM users u JOIN posts p ON u.id = p.user_id
       ORDER BY p.id ASC`)
-    res.json(result.rows);
+
+      const pageNum = Number(req.params.page)
+
+      const tenPost = result.rows.slice(pageNum*10-10, pageNum*10)
+
+      const post = tenPost.map((row) => {
+        return {
+          id: row[0],
+          name: row[1],
+          title: row[2],
+          likes: row[3],
+          views: row[4],
+          created_at: row[5],
+        }
+      })
+    res.json(post);
     
-    for (let i = 0; i < 10; i++) {
-      console.log(result.rows[i][0]);
-      console.log(result.rows[i][1]);
-      console.log(result.rows[i][2]);
-      console.log(result.rows[i][3]);
-      console.log(result.rows[i][4]);
-      console.log(result.rows[i][5]);
-    }
-
-    // const postId = result.rows[0][0]
-    // const userName = result.rows[0][1]
-    // const postTitle = result.rows[0][2]
-    // const postLikes = result.rows[0][3]
-    // const postViews = result.rows[0][4]
-    // const postCreated_at = result.rows[0][5]
-
-    // console.log(postId);
-    // console.log(userName);
-    // console.log(postTitle);
-    // console.log(postLikes);
-    // console.log(postViews);
-    // console.log(postCreated_at);
-    
-
-    // console.log("쿼리 결과:", result.rows[0][1]);
-    
-  } catch (err) {
-    console.error("DB 연결 또는 쿼리 에러:", err);
-    res.status(500).json({ error: 'DB 오류', message: err.message });
-  } finally {
-    if (connection) await connection.close();
-  }
-});
-
-app.get('/comments', async (req, res) => {
-  let connection;
-
-  try {
-    connection = await oracledb.getConnection(dbConfig);
-    const result = await connection.execute('SELECT * FROM COMMENTS');
-    res.json(result.rows);
-  
     
   } catch (err) {
     console.error("DB 연결 또는 쿼리 에러:", err);
@@ -99,34 +57,4 @@ app.get('/comments', async (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`서버 실행 중: http://localhost:${PORT}`);
-});
-
-
-
-app.get('/', async (req, res) => {
-    let connection;
-
-  try {
-    connection = await oracledb.getConnection(dbConfig);
-    const result = await connection.execute(
-      `SELECT p.id, u.name, p.title, p.likes, p.views, p.created_at
-      FROM users u JOIN posts p ON u.id = p.user_id
-      ORDER BY p.id ASC`)
-      const limit = Math.min(10, result.rows.length);
-      for (let i = 0; i < limit; i++) {
-      console.log(result.rows[i][0]);
-      console.log(result.rows[i][1]);
-      console.log(result.rows[i][2]);
-      console.log(result.rows[i][3]);
-      console.log(result.rows[i][4]);
-      console.log(result.rows[i][5]);
-    }
-    res.json(result.rows);
-    
-  } catch (err) {
-    console.error("DB 연결 또는 쿼리 에러:", err);
-    res.status(500).json({ error: 'DB 오류', message: err.message });
-  } finally {
-    if (connection) await connection.close();
-  }
 });
