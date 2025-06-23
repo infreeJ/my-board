e57225e58fa5786dc9de806113b497d0738aadc0
infreeJ <kasious97@naver.com>
2025-06-23 19:15:53 +0900

    글 컨트롤러 절반 구현

---
+++ backend/controller/writeController.js
@@ -0,0 +1,28 @@
+const { oracledb, dbConfig } = require('../db')
+
+const writePost = async (req, res) => {
+  let connection;
+
+  try {
+    connection = await oracledb.getConnection(dbConfig);
+    const { title, text } = req.body;
+
+    const result = await connection.execute(
+      `INSERT INTO POSTS
+      VALUES(posts_seq.NEXTVAL, :title, :text, 0, 0, SYSDATE, :userId)`,
+      [title, text, userId],
+      { autoCommit: true })
+
+    if (result.rowsAffected == 1) {
+      res.json({ success: true })
+    }
+
+  } catch (err) {
+    console.error("DB 연결 또는 쿼리 에러:", err);
+    res.status(500).json({ error: 'DB 오류', message: err.message });
+  } finally {
+    if (connection) await connection.close();
+  }
+}
+
+module.exports = { writePost };
--- frontend/src/pages/WritePage.tsx
+++ frontend/src/pages/WritePage.tsx
@@ -1,20 +1,53 @@
+import { useState } from 'react'
 import '../css-file/WritePage.css'
+import { useNavigate } from 'react-router-dom';
 
 
 function WritePage() {
 
+  const nav = useNavigate();
+
+const [ title, setTitle ] = useState("");
+const [ text, setText ] = useState("");
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
 
 
-  return(
+    try{
+      const response = await fetch("http://localhost:5000/write", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"},
+        body: JSON.stringify({ title, text })
+      })
+
+      const result = await response.json();
+
+      if(result.success){
+        alert("작성 완료");
+        nav("/post/page/1")
+      } else {
+        alert("작성 실패")
+      }
+    } catch(err) {
+      console.error("로그인 요청 중 에러:", err);
+    }
+
+
+  } 
+
+
+  return (
 
     <>
-    <div className="write-wrapper">
-        <input type="text" placeholder='제목을 입력하세요'/>
-      <textarea name="" id="" placeholder='본문을 입력하세요'></textarea>
-      <button className='btn' type="submit">작성</button>
-    </div>
-    
-    
+      <form className="write-wrapper" onSubmit={handleSubmit}>
+        <input type="text" placeholder='제목을 입력하세요' value={title} onChange={(e) => setTitle(e.target.value)}/>
+        <textarea placeholder='본문을 입력하세요' value={text} onChange={(e) => setText(e.target.value)}></textarea>
+        <button className='btn' type="submit">작성</button>
+      </form>
+
+
     </>
   )
 }
